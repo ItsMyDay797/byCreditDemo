3017a222319a4de3944b4ca77cb07b9ecfdc084e
ItsMyDay797 <m.rus1298@gmail.com>
2025-04-06 15:27:40 +0300

    Initial Commit

---
+++ byCreditDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  byCreditDemo
+//
+//  Created by Марк Русаков on 6.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
